f59c8440c276daaccb1b06f679001db98bc74921
Vineeth <vineeth.24cs181@sode-edu.in>
2026-07-29 00:01:01 +0530

    Time: 2 ms (92.11%), Space: 47.7 MB (57.45%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[insertPos];
+                nums[insertPos] = nums[i];
+                nums[i] = temp;
+                insertPos++;
+            }
+        }
+    }
+}
