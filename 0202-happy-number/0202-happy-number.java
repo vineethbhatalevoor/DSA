6363aadc28a2555688376a50e8f947bf1ab3dafe
Vineeth <vineeth.24cs181@sode-edu.in>
2026-07-30 23:53:23 +0530

    Time: 0 ms (100%), Space: 41.5 MB (99.81%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+        return fast == 1;
+    }
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            totalSum += digit * digit;
+            n /= 10;       }
+        return totalSum;
+    }
+}
