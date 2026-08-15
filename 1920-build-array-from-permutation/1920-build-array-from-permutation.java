4a856064f199d839d023527561b63583d4b4d6b4
Vineeth <vineeth.24cs181@sode-edu.in>
2026-08-15 18:31:19 +0530

    Time: 1 ms (99.56%), Space: 47 MB (73.49%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] buildArray(int[] nums) {
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int oldValue = nums[i];
-            int newValue = nums[nums[i]] % 1000; 
+        int[] ans = new int[n];
 
-            nums[i] = oldValue + 1000 * newValue;
-        }
         for (int i = 0; i < n; i++) {
-            nums[i] /= 1000;
+            ans[i] = nums[nums[i]];
         }
 
-        return nums;
+        return ans;
     }
 }
